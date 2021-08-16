bccaf04746d3b84ebda9ead2639dcd186e44300e
Stevie <65572293+stevie170@users.noreply.github.com>
2021-08-16 10:21:20 -0500

    Update password-generator.py
    added a few comments to the code

--- password-generator.py
+++ password-generator.py
@@ -1,4 +1,7 @@
 # password generator: user is prompted for a password length and given a password of that length
+# This is an application of python and does not necessarily generate secure passwords. A secure password is one that is long and easily remembered, such as a passphrase.
+# Passwords such as these (random characters of the desired password length) are more likely to be written down, and thus less secure.
+# A good password manager can be a great tool if you want to manage passwords you have a hard time remembering without writing them down.
 
 import random
 
